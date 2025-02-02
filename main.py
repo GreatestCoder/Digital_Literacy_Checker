995b7d873f0a6fc6754e6c1583640d3c44ee4c53
GreatestCoder <namansharma9858581009@gmail.com>
2025-02-02 19:54:09 +0530

    final changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import joblib as jb
 
-df = pd.read_csv(r"C:\Users\LENOVO\Documents\AI\MachineLearning\Digital_Literacy\data\digital_literacy_dataset.csv")
+df = pd.read_csv(r"data/digital_literacy_dataset.csv")
 df.drop(columns=["User_ID"], axis=1, inplace=True)
 df['Education_Level'].fillna('Unknown', inplace=True)
 
@@ -18,7 +18,6 @@ for col in categorical_columns:
 
 
 x = df.drop(columns=["Overall_Literacy_Score"])
-print(x.columns)
 y = df["Overall_Literacy_Score"]
 X_Train, X_Test, Y_Train, Y_Test = train_test_split(x, y, test_size=0.2, random_state=42)
 
@@ -38,6 +37,6 @@ input_values = [25, 1, 2, 1, 2, 1, 8, 7, 9, 10, 9, 10, 5, 2.5, 8, 4, 2, 7, 9, 8,
 new_pred = model.predict([input_values])
 print(f"Overall_Literacy_Score: {round(new_pred[0])}")
 
-jb.dump(model, r"C:\Users\LENOVO\Documents\AI\MachineLearning\Digital_Literacy\Model.pkl")
-jb.dump(le, r'C:\Users\LENOVO\Documents\AI\MachineLearning\Digital_Literacy\LabelEncoder.pkl')
+jb.dump(model, 'Model.pkl')
+jb.dump(le, 'LabelEncoder.pkl')
 
